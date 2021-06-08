7b847cc552208f9c54bfa9e489d593de06636155
renchaoyang <1747715859@qq.com>
2021-06-08 14:57:09 +0800

    用户工具类

---
+++ src/main/java/cn/rongcloud/moment/server/common/utils/UserHolder.java
@@ -0,0 +1,25 @@
+package cn.rongcloud.moment.server.common.utils;
+
+import cn.rongcloud.moment.server.common.jwt.JwtUser;
+
+/**
+ * @author renchaoyang
+ * @date 2021/6/7
+ */
+public class UserHolder {
+
+    public static final ThreadLocal<JwtUser> userHolder = new ThreadLocal();
+
+    public static void setUser(JwtUser jwtUser){
+        userHolder.set(jwtUser);
+    }
+
+    public static JwtUser getUser() {
+        return userHolder.get();
+    }
+
+    public static String getUid() {
+        return userHolder.get().getUserId();
+    }
+
+}
