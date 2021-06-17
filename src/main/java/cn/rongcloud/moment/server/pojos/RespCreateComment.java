3ec6df37afa02f220d14242bf1840e69ed929ffb
renchaoyang <1747715859@qq.com>
2021-06-17 14:38:59 +0800

    评论返回时间戳

--- src/main/java/cn/rongcloud/moment/server/pojos/RespCreateComment.java
+++ src/main/java/cn/rongcloud/moment/server/pojos/RespCreateComment.java
@@ -3,6 +3,8 @@ package cn.rongcloud.moment.server.pojos;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import java.util.Date;
+
 /**
  * @author renchaoyang
  * @date 2021/6/9
@@ -14,7 +16,7 @@ public class RespCreateComment {
     private String commentId;
 
     @JsonProperty("create_dt")
-    private String createDt;
+    private Date createDt;
 
     private String content;
 
